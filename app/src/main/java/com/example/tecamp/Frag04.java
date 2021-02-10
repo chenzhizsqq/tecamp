4f200075c0d0d290922b538aeab7c16b17e64ceb
chenzhi <chenzhi@qq.com>
2021-02-10 11:32:06 +0900

    2021/02/10 已经修复好Frag04页面中，最上时间的显示问题

--- app/src/main/java/com/example/tecamp/Frag04.java
+++ app/src/main/java/com/example/tecamp/Frag04.java
@@ -75,17 +75,15 @@ public class Frag04 extends Fragment implements
         try {
 
             mDateManager.addDay(_addDay);
-            TextView TV_frag04_current_date = tableLayout_menu.findViewById(R.id.frag04_current_date);
-            TV_frag04_current_date.setText(mDateManager.getYear()
-                    + "/" + mDateManager.getMonth()
-                    + "/" + mDateManager.getDay()
-            );
 
             for (int i = 0; i < mSrcDays; i++) {
                 arrayDate[i].addDay(_addDay);
                 textView_Src_Days[i].setText(arrayDate[i].getYear() + "/" + arrayDate[i].getMonth() + "/" + arrayDate[i].getDay());
                 //textView_Src_Days[i].setText(arrayDate[i].getYMD());
             }
+
+            TextView TV_frag04_current_date = tableLayout_menu.findViewById(R.id.frag04_current_date);
+            TV_frag04_current_date.setText(arrayDate[0].getYear() + "/" + arrayDate[0].getMonth() + "/" + arrayDate[0].getDay());
 
             for (int i = 0; i < mSrcDays; i++) {
                 //Log.e(TAG, "onCreate: arrayDateManager:" + arrayDate[i].getYMD());
@@ -131,7 +129,7 @@ public class Frag04 extends Fragment implements
         }
     }
 
-    @SuppressLint({"WrongConstant", "SetTextI18n"})
+    //@SuppressLint({"WrongConstant", "SetTextI18n"})
     @Nullable
     @Override
     public View onCreateView(
@@ -141,104 +139,132 @@ public class Frag04 extends Fragment implements
         View view = inflater.inflate(R.layout.frag04_layout, container, false);
         ButterKnife.bind(this, view);
 
-        tableLayout_menu = (TableLayout) view.findViewById(R.id.Frag04_TableLayout_menu);
-        tableLayout_table = (TableLayout) view.findViewById(R.id.Frag04_TableLayout_table);
+        try {
 
-        textView_Src_Days[0] = tableLayout_table.findViewById(R.id.Frag04_textView_date_0);
-        textView_Src_Days[1] = tableLayout_table.findViewById(R.id.Frag04_textView_date_1);
-        textView_Src_Days[2] = tableLayout_table.findViewById(R.id.Frag04_textView_date_2);
-        textView_Src_Days[3] = tableLayout_table.findViewById(R.id.Frag04_textView_date_3);
-        textView_Src_Days[4] = tableLayout_table.findViewById(R.id.Frag04_textView_date_4);
-        textView_Src_Days[5] = tableLayout_table.findViewById(R.id.Frag04_textView_date_5);
-        textView_Src_Days[6] = tableLayout_table.findViewById(R.id.Frag04_textView_date_6);
-        for (int i = 0; i < mSrcDays; i++) {
-            textView_Src_Days[i].setText(arrayDate[i].getMonth() + "/" + arrayDate[i].getDay());
-            textView_Src_Days[i].setText(arrayDate[i].getYMD());
+            tableLayout_menu = (TableLayout) view.findViewById(R.id.Frag04_TableLayout_menu);
+            tableLayout_table = (TableLayout) view.findViewById(R.id.Frag04_TableLayout_table);
+
+
+            textView_Src_Days[0] = tableLayout_table.findViewById(R.id.Frag04_textView_date_0);
+            textView_Src_Days[1] = tableLayout_table.findViewById(R.id.Frag04_textView_date_1);
+            textView_Src_Days[2] = tableLayout_table.findViewById(R.id.Frag04_textView_date_2);
+            textView_Src_Days[3] = tableLayout_table.findViewById(R.id.Frag04_textView_date_3);
+            textView_Src_Days[4] = tableLayout_table.findViewById(R.id.Frag04_textView_date_4);
+            textView_Src_Days[5] = tableLayout_table.findViewById(R.id.Frag04_textView_date_5);
+            textView_Src_Days[6] = tableLayout_table.findViewById(R.id.Frag04_textView_date_6);
+            for (int i = 0; i < mSrcDays; i++) {
+                textView_Src_Days[i].setText(arrayDate[i].getMonth() + "/" + arrayDate[i].getDay());
+                textView_Src_Days[i].setText(arrayDate[i].getYMD());
+            }
+
+            TextView TV_frag04_current_date = tableLayout_menu.findViewById(R.id.frag04_current_date);
+            TV_frag04_current_date.setText(arrayDate[0].getYear() + "/" + arrayDate[0].getMonth() + "/" + arrayDate[0].getDay());
+
+            TableRow tableRow;
+            int roomIndex = 0;
+            for (Map.Entry<Integer, String> entry : Rooms.mapRoomNames.entrySet()) {
+                tableRow = new TableRow(getActivity());
+                int color = getResources().getColor(R.color.navy, getActivity().getTheme());
+                tableRow.setBackgroundColor(color);
+
+
+                TextView textViewNo = createTextView(Rooms.getRoomName(entry.getKey()), Rooms.getRoomName(entry.getKey()));
+                tableRow.addView(textViewNo);
+
+                for (int dayIndex = 0; dayIndex < mSrcDays; dayIndex++) {
+
+                    String tag = entry.getKey() + "_date_" + dayIndex;
+                    TextView textView = createTextView("", tag);
+                    textView.setTextColor(Color.BLACK);
+
+                    tagArrayList.add(tag);
+
+                    tableRow.addView(textView, dayIndex + 1);
+                }
+                tableLayout_table.addView(tableRow, roomIndex + 1);
+                roomIndex++;
+            }
+
+
+            //「検索日　選択」セット begin
+
+
+            Button mOpenDialog_booking_date = tableLayout_menu.findViewById(R.id.pick_date_search_date);
+            mOpenDialog_booking_date.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    DatePick dialog = new DatePick();
+                    dialog.setTargetFragment(Frag04.this, 1);
+                    dialog.show(getFragmentManager(), "booking date Dialog");
+                }
+            });
+            //「検索日　選択」セット end
+
+
+
+
+            Button buttonNext = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonNext);
+            buttonNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+
+                        Frag04DataUpdate(1);
+                    }catch (Exception e){
+                        Log.e(TAG, "onClick: ",e );
+                    }
+                }
+            });
+
+            Button buttonPrev = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonPrev);
+            buttonPrev.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    try {
+
+                        Frag04DataUpdate(-1);
+                    }catch (Exception e){
+                        Log.e(TAG, "onClick: ",e );
+                    }
+                }
+            });
+
+
+            Button buttonNextWeek = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonNextWeek);
+            buttonNextWeek.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    try {
+
+                        Frag04DataUpdate(7);
+                    }catch (Exception e){
+                        Log.e(TAG, "onClick: ",e );
+                    }
+                }
+            });
+
+            Button buttonPrevWeek = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonPrevWeek);
+            buttonPrevWeek.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    try {
+
+                        Frag04DataUpdate(-7);
+                    }catch (Exception e){
+                        Log.e(TAG, "onClick: ",e );
+                    }
+                }
+            });
+
+            //Data Update
+            Frag04DataUpdate(0);
+        }catch (Exception e){
+            Log.e(TAG, "onCreateView: ",e );
         }
-
-
-        TableRow tableRow;
-        int roomIndex = 0;
-        for (Map.Entry<Integer, String> entry : Rooms.mapRoomNames.entrySet()) {
-            tableRow = new TableRow(getActivity());
-            int color = getResources().getColor(R.color.navy, getActivity().getTheme());
-            tableRow.setBackgroundColor(color);
-
-
-            TextView textViewNo = createTextView(Rooms.getRoomName(entry.getKey()), Rooms.getRoomName(entry.getKey()));
-            tableRow.addView(textViewNo);
-
-            for (int dayIndex = 0; dayIndex < mSrcDays; dayIndex++) {
-
-                String tag = entry.getKey() + "_date_" + dayIndex;
-                TextView textView = createTextView("", tag);
-                textView.setTextColor(Color.BLACK);
-
-                tagArrayList.add(tag);
-
-                tableRow.addView(textView, dayIndex + 1);
-            }
-            tableLayout_table.addView(tableRow, roomIndex + 1);
-            roomIndex++;
-        }
-
-
-        //「検索日　選択」セット begin
-
-
-        Button mOpenDialog_booking_date = tableLayout_menu.findViewById(R.id.pick_date_search_date);
-        mOpenDialog_booking_date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                DatePick dialog = new DatePick();
-                dialog.setTargetFragment(Frag04.this, 1);
-                dialog.show(getFragmentManager(), "booking date Dialog");
-            }
-        });
-        //「検索日　選択」セット end
-
-
-
-
-        Button buttonNext = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonNext);
-        buttonNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Frag04DataUpdate(1);
-            }
-        });
-
-        Button buttonPrev = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonPrev);
-        buttonPrev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Frag04DataUpdate(-1);
-            }
-        });
-
-
-        Button buttonNextWeek = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonNextWeek);
-        buttonNextWeek.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Frag04DataUpdate(7);
-            }
-        });
-
-        Button buttonPrevWeek = (Button) tableLayout_menu.findViewById(R.id.Frag04ButtonPrevWeek);
-        buttonPrevWeek.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Frag04DataUpdate(-7);
-            }
-        });
-
-        //Data Update
-        Frag04DataUpdate(0);
 
         return view;
     }
