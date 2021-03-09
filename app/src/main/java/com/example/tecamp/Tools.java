32c9ca476c2616b3ae6de163480c16402ac70418
chenzhi <chenzhi@qq.com>
2021-03-09 10:22:48 +0900

    添加不同日子，相差的天数

--- app/src/main/java/com/example/tecamp/Tools.java
+++ app/src/main/java/com/example/tecamp/Tools.java
@@ -2,6 +2,9 @@ package com.example.tecamp;
 
 import android.util.Log;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Tools {
     private static final String TAG = "Tools";
 
@@ -42,5 +45,56 @@ public class Tools {
         if(_data.isEmpty())return 0;
         String str = _data.substring(6, 8);
         return Integer.parseInt(str);
+    }
+
+
+    static int getDiffDays(Calendar calendar1, Calendar calendar2) {
+        //==== ミリ秒単位での差分算出 ====//
+        long diffTime = calendar1.getTimeInMillis() - calendar2.getTimeInMillis();
+
+
+        //==== 日単位に変換 ====//
+        int MILLIS_OF_DAY = 1000 * 60 * 60 * 24;
+        int diffDays = (int)(diffTime / MILLIS_OF_DAY);
+
+        return diffDays;
+    }
+
+    /*
+    * 二つの日の違いの日数
+    * */
+    public static int differentDays(Date date1, Date date2)
+    {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        int day1= cal1.get(Calendar.DAY_OF_YEAR);
+        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+
+        int year1 = cal1.get(Calendar.YEAR);
+        int year2 = cal2.get(Calendar.YEAR);
+        if(year1 != year2)
+        {
+            int timeDistance = 0 ;
+            for(int i = year1 ; i < year2 ; i ++)
+            {
+                if(i%4==0 && i%100!=0 || i%400==0)
+                {
+                    timeDistance += 366;
+                }
+                else
+                {
+                    timeDistance += 365;
+                }
+            }
+
+            return timeDistance + (day2-day1) ;
+        }
+        else
+        {
+            return day2-day1;
+        }
     }
 }
